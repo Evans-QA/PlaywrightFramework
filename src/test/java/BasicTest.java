1e31df1fe7cbd77a1f448a7a3c3634aadf646c65
Kevin <krawczynskikewin@gmail.com>
2026-09-08 22:18:26 +0200

    feat: add basic login automation test with Playwright

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -2,6 +2,7 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
 import org.testng.annotations.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
@@ -16,17 +17,21 @@ public class BasicTest {
     public void DemoTest() {
 
         Playwright playwright = Playwright.create();
-        //Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
        //Browser browser = playwright.chromium().launch();
-        Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        //Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
         Page page = browser.newPage();
         page.navigate("https://eventhub.rahulshettyacademy.com/");
         System.out.println(page.title());
         assertEquals("EventHub — Discover & Book Events", page.title());
         assertThat(page).hasTitle("EventHub — Discover & Book Events");
+        page.getByPlaceholder("Email").fill("rahulshetty1@yahoo.com");
+        page.getByLabel("Password").fill("Magiclife1!");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign In")).click();
 
-        browser.close();
-        playwright.close();
+
+        //browser.close();
+        //playwright.close();
 
     }
 }
